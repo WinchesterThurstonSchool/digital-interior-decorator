65e987e7bdd12b098177cba10029c5955b207fc9
Jerry <zhangs@winchesterthurston.org>
2020-05-20 22:40:23 +0800

    Refractor upload.php.

--- api/upload.php
+++ api/upload.php
@@ -1,5 +1,5 @@
 <?php 
-$root = "../.."; 
+$root = "../"; 
 
 $error_code = $_FILES['image_input']['error']; 
 
@@ -13,20 +13,20 @@ if (!isset($error_code)) {
 // check if upload failed
 if ($error_code !== 0) {
   // redirect to home page and pass error code
-  header("Location: {$root}/?error=".$error_code); 
+  header("Location: {$root}?error=".$error_code); 
   exit(); 
 }
 
 // handle the uploaded image
 if (($_FILES['image_input']['name'] != "")) {
-  $target_dir = $root."/uploaded/";
+  $target_dir = $root."uploaded/";
   $file = $_FILES['image_input']['name'];
   $path = pathinfo($file);
   $ext = $path['extension'];
 
   // return error when file extension is invalid
   if ($ext !== "jpg" && $ext !== "jpeg" && $ext !== "png") {
-    header("Location: {$root}/?error=-1"); 
+    header("Location: {$root}?error=-1"); 
     exit; 
   }
 
@@ -41,12 +41,12 @@ if (($_FILES['image_input']['name'] != "")) {
   move_uploaded_file($temp_name,$path_filename_ext);
   
   // redirect user to the result page
-  header("Location: {$root}/processing.html?id=".$filename."&ext=".$ext); 
+  header("Location: {$root}processing.html?id=".$filename."&ext=".$ext); 
   exit(); 
 }
 
 // fallback redirect
-header("Location: {$root}/"); 
+header("Location: {$root}"); 
 
 // generate a random string
 function generateRandomString($length = 16) {
